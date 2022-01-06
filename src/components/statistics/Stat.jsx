e7f9d480dca3cde6e362c6b7bc97d43a146fa1c0
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2022-01-06 16:24:27 +0100

    cleanup

--- src/components/statistics/Stat.jsx
+++ src/components/statistics/Stat.jsx
@@ -6,50 +6,50 @@ export default function Stat() {
     return (
         <div className="before-footer">
             <div className="sypnosis">
-            <div className="sypnosis-heading" data-aos="fade-down" data-aos-delay="100">
-                <h2>How we can help you</h2>
-            </div>
-            <div className="sypnosis-content">
-
-                <div data-aos="fade-left" data-aos-delay="200">
-                <h5>Fire Protection & Fireproofing</h5>
-                <p>Design of fire protection systems for buildings and structures Procurement & supply of fire protection & fireproofing materials Procurement & supply of fireproofing equipment & accessories Installation of fire protection systems Installation of fireproofing</p>
-                <button className="btnz"><Link to ="/contact" className="btn-blog">Learn More</Link></button>
+                <div className="sypnosis-heading" data-aos="fade-down" data-aos-delay="100">
+                    <h2>How we can help you</h2>
                 </div>
+                <div className="sypnosis-content">
 
-                <div data-aos="fade-left" data-aos-delay="300">
-                <h5>Procurement Services</h5>
-                <p>Design of fire protection systems for buildings and structures Procurement & supply of fire protection & fireproofing materials Procurement & supply of fireproofing equipment & accessories Installation of fire protection systems Installation of fireproofing</p>
-                <button className="btnz"><Link to ="/contact" className="btn-blog">Learn More</Link></button>
+                    <div data-aos="fade-left" data-aos-delay="200">
+                    <h5>Fire Protection & Fireproofing</h5>
+                    <p>Design of fire protection systems for buildings and structures Procurement & supply of fire protection & fireproofing materials Procurement & supply of fireproofing equipment & accessories Installation of fire protection systems Installation of fireproofing</p>
+                    <button className="btnz"><Link to ="/contact" className="btn-blog">Learn More</Link></button>
+                    </div>
+
+                    <div data-aos="fade-left" data-aos-delay="300">
+                    <h5>Procurement Services</h5>
+                    <p>Design of fire protection systems for buildings and structures Procurement & supply of fire protection & fireproofing materials Procurement & supply of fireproofing equipment & accessories Installation of fire protection systems Installation of fireproofing</p>
+                    <button className="btnz"><Link to ="/contact" className="btn-blog">Learn More</Link></button>
+                    </div>
+
+                    <div data-aos="fade-left" data-aos-delay="400">
+                    <h5>Supply of Technical Manpower</h5>
+                    <p>Design of fire protection systems for buildings and structures Procurement & supply of fire protection & fireproofing materials Procurement & supply of fireproofing equipment & accessories Installation of fire protection systems Installation of fireproofing</p>
+                    <button className="btnz"><Link to ="/contact" className="btn-blog">Learn More</Link></button>
+                    </div>
+
                 </div>
-
-                <div data-aos="fade-left" data-aos-delay="400">
-                <h5>Supply of Technical Manpower</h5>
-                <p>Design of fire protection systems for buildings and structures Procurement & supply of fire protection & fireproofing materials Procurement & supply of fireproofing equipment & accessories Installation of fire protection systems Installation of fireproofing</p>
-                <button className="btnz"><Link to ="/contact" className="btn-blog">Learn More</Link></button>
-                </div>
-
-            </div>
             </div>
 
             <div className="statistics">
-            <div className="statistics-heading" data-aos="flip-down" data-aos-delay="100"> 
-                <h2>More than 10 Years of Experience</h2>
-            </div>
-            <div className="statistics-content">
-                <div className="stat-icon" data-aos="fade-right" data-aos-delay="100">
-                <i className="fas fa-suitcase"></i>
-                <span>221 projects completed</span>
+                <div className="statistics-heading" data-aos="flip-down" data-aos-delay="100"> 
+                    <h2>More than 10 Years of Experience</h2>
                 </div>
-                <div  className="stat-icon" data-aos="flip-up" data-aos-delay="100">
-                <i className="fas fa-award"></i>
-                <span>12 awards won</span>
+                <div className="statistics-content">
+                    <div className="stat-icon" data-aos="fade-right" data-aos-delay="100">
+                    <i className="fas fa-suitcase"></i>
+                    <span>221 projects completed</span>
+                    </div>
+                    <div  className="stat-icon" data-aos="flip-up" data-aos-delay="100">
+                    <i className="fas fa-award"></i>
+                    <span>12 awards won</span>
+                    </div>
+                    <div  className="stat-icon" data-aos="fade-left" data-aos-delay="100">
+                    <i className="fas fa-thumbs-up"></i>
+                    <span>58 satisfied clients</span>
+                    </div>
                 </div>
-                <div  className="stat-icon" data-aos="fade-left" data-aos-delay="100">
-                <i className="fas fa-thumbs-up"></i>
-                <span>58 satisfied clients</span>
-                </div>
-            </div>
             </div>
 
             <div className="contact-us">
