ad856784a9b34dfb07475a5b66e3b975d4a51411
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2022-01-30 07:15:27 +0100

    Removing Security Risk

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -9,7 +9,7 @@ export default function Post({_id, photo, desc, title, categories, createdAt}) {
     // const PF = 'https://th.bing.com/th/id/R.b9d0042c17cfae5240eac6b369c32455?rik=NNBlxgNPX%2f5pIQ&pid=ImgRaw&r=0'
     return (
         <>
-            <Link to = {`/post/${_id}`} className="post link" key={_id} style={{padding: "0", margin: "0"}} className="post">
+            <Link to = {`/post/${_id}`} className="post link" key={_id} style={{padding: "0", margin: "0"}}>
                 {photo && (<img className="postImg" src={`${PF}${photo}`} alt="" />)}
                 {/* {photo && (<img className="postImg" src={PF} alt="" />)} */}
                 <div className="postInfo">
